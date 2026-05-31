9519fc929bc469be8ff319cd3deb032ec2930d83
kolenka-matka <70626173+kolenka-matka@users.noreply.github.com>
2026-05-31 23:28:14 +0300

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,7 +3,6 @@ using MyApp.Models;
 
 namespace MyApp.Controllers;
 
-// Контроллер №1 (для модели Product)
 [ApiController]
 [Route("api/[controller]")]
 public class ProductsController : ControllerBase
