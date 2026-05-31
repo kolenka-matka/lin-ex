ab32bba1d39ce59c1ed2a6bfb047d335143d4b29
kolenka-matka <70626173+kolenka-matka@users.noreply.github.com>
2026-05-31 23:29:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Регистрируем контроллеры и Swagger (удобно для проверки API в браузере)
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Swagger включаем всегда, чтобы на зачёте можно было открыть /swagger и показать API
 app.UseSwagger();
 app.UseSwaggerUI();
 
